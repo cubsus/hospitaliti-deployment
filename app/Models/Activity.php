fca8e3626f30689db5bdbc3dfa6ede1ca3b85f1a
Andrei Feraru <feraandrei1@gmail.com>
2025-12-04 18:23:27 +0200

    changed causerInfo() of Activity be showed correctly

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -35,7 +35,7 @@ class Activity extends SpatieActivity
     public function causerInfo(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->causer->full_name ?? 'System'
+            get: fn() => $this->causer->name ?? 'System'
         );
     }
 
